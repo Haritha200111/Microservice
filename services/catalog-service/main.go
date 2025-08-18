7587c24139a47194749c59610e8bd8b111b6a727
Haritha <haritha.ravichandran@multicorewareinc.com>
2025-08-18 14:52:53 +0530

    test added

--- services/catalog-service/main.go
+++ services/catalog-service/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Catalog Service: product list")
+	fmt.Fprintf(w, "Catalog Service: product listeeee")
 }
 
 func main() {
